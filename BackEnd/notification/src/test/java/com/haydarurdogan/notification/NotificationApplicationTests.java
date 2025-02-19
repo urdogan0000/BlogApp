403a8bebc12729cb61554f92388dc148fff07383
urdogan0000 <hydrurdgn@gmail.com>
2025-02-19 13:55:54 +0300

    initialize

---
+++ BackEnd/notification/src/test/java/com/haydarurdogan/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haydarurdogan.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
